a94d3c06dafef17d6f6f85bcd532040f9078e217
Henrik Nordstrom <henrik.nordstrom@addiva.se>
2022-12-06 15:56:07 +0100

    Automate saving of configuration changes on linux-menuconfig

--- runme.d/linux-config.sh
+++ runme.d/linux-config.sh
@@ -2,3 +2,5 @@ echo "Configuring the kernel with $ROOTDIR/configs/linux/cn913x_additions.config
 cd $ROOTDIR/build/linux
 #make defconfig
 ./scripts/kconfig/merge_config.sh arch/arm64/configs/defconfig $ROOTDIR/configs/linux/cn913x_additions.config
+make savedefconfig
+mv defconfig defconfig_cn913x_additions
--- runme.d/linux-menuconfig.sh
+++ runme.d/linux-menuconfig.sh
@@ -1,3 +1,25 @@
 echo "Manually configuring the kernel"
 cd $ROOTDIR/build/linux
 make menuconfig
+make savedefconfig
+mv defconfig defconfig_manual
+./scripts/diffconfig defconfig_cn913x_additions defconfig_manual | while read opt val; do
+	plus="$(echo "$opt" |cut -c-1)"
+	opt="$(echo "$opt" |cut -c2-)"
+	case "$plus" in
+	"-")
+		val="n"
+		;;
+	esac
+	echo "${opt}=${val}"
+done > defconfig.diff
+cat defconfig.diff
+echo "Enter tag to save  changes to defconfig (blank aborts)"
+echo -n "Tag: "
+read ans
+if [ "x$ans" != "x" ]; then
+    echo "" >> $ROOTDIR/configs/linux/cn913x_additions.config
+    echo "# $ans" >> $ROOTDIR/configs/linux/cn913x_additions.config
+    echo "" >> $ROOTDIR/configs/linux/cn913x_additions.config
+    cat defconfig.diff  >> $ROOTDIR/configs/linux/cn913x_additions.config
+fi
