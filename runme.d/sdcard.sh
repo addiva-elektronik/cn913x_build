8cf60cdf3227d3c2a36f076ca83770c011660fd9
Henrik Nordstrom <henrik.nordstrom@addiva.se>
2022-12-07 13:08:45 +0100

    sdcard: Correct partition offsets

--- runme.d/sdcard.sh
+++ runme.d/sdcard.sh
@@ -9,10 +9,10 @@ unit: sectors
 first-lba: 128
 
 # Reserved partitions
-start=440 size=6MB type=8DA63339-0007-60C0-C436-083AC8230908 name=Bootloader attrs=RequiredPartition
-start=16384 size=1MB type=8DA63339-0007-60C0-C436-083AC8230908 name=Environment attrs=RequiredPartition
+start=4096 size=6MiB type=8DA63339-0007-60C0-C436-083AC8230908 name=Bootloader attrs=RequiredPartition
+start=16384 size=1MiB type=8DA63339-0007-60C0-C436-083AC8230908 name=Environment attrs=RequiredPartition
 # aarch64 root partition
-start=64MB name=Linux type=B921B045-1DF0-41C3-AF44-4C6F280D3FAE bootable
+start=64MiB name=Linux type=B921B045-1DF0-41C3-AF44-4C6F280D3FAE bootable
 %PTBL%
 ROOTPART=$(sfdisk --part-uuid tmp/ubuntu-core.img 3)
 sfdisk -d tmp/ubuntu-core.img
