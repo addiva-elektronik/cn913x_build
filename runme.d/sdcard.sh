337d3b4014fa99a1399a2d62a78c465b054c7956
Henrik Nordstrom <henrik.nordstrom@addiva.se>
2022-12-07 12:24:56 +0100

    Assign proper partition GUIDs for bootloader and u-boot environment

--- runme.d/sdcard.sh
+++ runme.d/sdcard.sh
@@ -8,9 +8,11 @@ label: gpt
 unit: sectors
 first-lba: 128
 
-start=440 size=6MB type=00000000-0000-0000-0000-000000000000 name=Bootloader attrs=RequiredPartition
-start=16384 size=1MB type=00000000-0000-0000-0000-000000000000 name=Environment attrs=RequiredPartition
-start=64MB name=Linux bootable
+# Reserved partitions
+start=440 size=6MB type=8DA63339-0007-60C0-C436-083AC8230908 name=Bootloader attrs=RequiredPartition
+start=16384 size=1MB type=8DA63339-0007-60C0-C436-083AC8230908 name=Environment attrs=RequiredPartition
+# aarch64 root partition
+start=64MB name=Linux type=B921B045-1DF0-41C3-AF44-4C6F280D3FAE bootable
 %PTBL%
 ROOTPART=$(sfdisk --part-uuid tmp/ubuntu-core.img 3)
 sfdisk -d tmp/ubuntu-core.img
