00bec2c9ccd3476c59699fc0a8ca66332a97b01b
Henrik Nordstrom <henrik.nordstrom@addiva.se>
2022-12-08 18:10:53 +0100

    And again correct sdcard partition layout

--- runme.d/sdcard.sh
+++ runme.d/sdcard.sh
@@ -6,7 +6,7 @@ freespace=512
 cp ubuntu-$release.ext4 tmp/ubuntu-core.ext4
 e2fsck -f tmp/ubuntu-core.ext4
 minsize=$(resize2fs -P tmp/ubuntu-core.ext4 | cut -d: -f2) # In 4K blocks
-size=$(((minsize * 4 + 512 * 1024 + 1024 * 1024 - 1 )/1024)) # in KB
+size=$(((minsize * 4 + 512 * 1024 + 1024 - 1 )/1024)) # in KB
 size=$(((size + 1023) / 1024 * 1024)) # Round to nearest MB
 resize2fs tmp/ubuntu-core.ext4 $((size*1024/4))
 
@@ -29,7 +29,7 @@ first-lba: 128
 start=$((boot_start*2)) size=$(((boot_end-env_size-boot_start)*2)) type=8DA63339-0007-60C0-C436-083AC8230908 name=Bootloader attrs=RequiredPartition
 start=$(((boot_end-env_size)*2)) size=$((env_size*2)) type=8DA63339-0007-60C0-C436-083AC8230908 name=Environment attrs=RequiredPartition
 # aarch64 root partition
-start=${root_start}M name=Linux type=B921B045-1DF0-41C3-AF44-4C6F280D3FAE bootable
+start=${root_start}M name=Linux type=B921B045-1DF0-41C3-AF44-4C6F280D3FAE attrs=LegacyBIOSBootable
 %PTBL%
 ROOTPART=$(sfdisk --part-uuid tmp/ubuntu-core.img 3)
 sfdisk -d tmp/ubuntu-core.img
